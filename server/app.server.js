a4334beaa8b48dc648bc59b05f3b6a8af0d91eea
ShapingSickness <isaachaloelrey13@gmail.com>
2017-06-26 21:28:03 -0600

    Added CORS middleware.
    Updated package.

--- server/app.server.js
+++ server/app.server.js
@@ -3,10 +3,17 @@ var app = express();
 var path = require("path");
 var rootPath = path.normalize(__dirname + "/../");
 var bodyParser = require("body-parser");
+var cors = require('express-cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(rootPath + "/app"));
+
+app.use(cors({
+    allowedOrigins: [
+        'haloapi.com'
+    ]
+}));
 
 app.get("*", function (req, res) {
     res.sendFile(rootPath + "/app/index.html");
